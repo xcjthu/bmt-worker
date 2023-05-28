df0f5e65b754a9cbfc6647a989a2721998e6988a
xiaocj <xcjthu@gmail.com>
2023-05-28 20:14:05 +0800

    Update metric.py

--- model/metric.py
+++ model/metric.py
@@ -9,7 +9,7 @@ import bmtrain as bmt
 # from rouge import Rouge
 
 def softmax_acc(score, label, acc_result):
-    if acc_result is None or acc_result["total"] > 25600:
+    if acc_result is None:
         acc_result = {'total': 0, 'right': 0}
     predict = torch.max(score, dim = 1)[1]
     acc_result['total'] += int(label.shape[0])
